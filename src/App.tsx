3aa338bbde8f9643c68a04de67d6c36bbdd5896f
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2022-09-27 06:19:55 +0300

    добавил счетчик

--- src/App.tsx
+++ src/App.tsx
@@ -15,21 +15,32 @@ const initialArr = [
   { id: 9, hidden: true, mined: true },
 ];
 
+// let user = initialArr.find(item => item.id == 1);
+// console.log(user);
+
 // console.log(initialArr);
 
 interface SquareProps {
   isHidden: any;
   setGameSquers: any;
   id: any;
+  setCount: any;
+  testCont: any;
 }
 
 function Square(props: SquareProps) {
-  const { isHidden, setGameSquers, id } = props;
+  const { isHidden, setGameSquers, id, setCount, testCont  } = props;
 
   const hiddenHandleClick = (e: any) => {
+    e.stopPropagation();
+    // if (!e.target.hidden) {
+    // }
+    setCount(testCont);
+
     setGameSquers((prevState: any) =>
       prevState.map((elem: any) => {
-        if (elem.id === id) {
+        if (elem.id === id && !elem.mined) {          
+
           return { ...elem, hidden: false };
         }
         return elem;
@@ -47,11 +58,18 @@ function Square(props: SquareProps) {
 
 function App() {
   const [gameSquers, setGameSquers] = useState<any>(initialArr);
+  const [count, setCount] = useState<any>(null);
 
-  console.log(gameSquers);
+  const testCont = gameSquers.filter((elem: any) =>(elem.hidden))
+
+console.log(count);
+
+  // console.log(gameSquers);
 
   const squers = gameSquers.map((elem: any) => (
     <Square
+    testCont={testCont}
+    setCount={setCount}
       id={elem.id}
       setGameSquers={setGameSquers}
       key={elem.id}
