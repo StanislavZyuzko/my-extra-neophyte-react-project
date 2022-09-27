7e352948e2f55b6fe30dd1b192a0fa2cdf82cccb
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2022-09-27 04:31:08 +0300

    подключил hidden

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,38 @@ import "./App.css";
 
 const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+const initialArr = [
+  { id: 1, hidden: true, mined: false },
+
+  { id: 2, hidden: true, mined: false },
+  { id: 3, hidden: true, mined: false },
+  { id: 4, hidden: true, mined: false },
+  { id: 5, hidden: true, mined: false },
+  { id: 6, hidden: true, mined: false },
+  { id: 7, hidden: true, mined: false },
+  { id: 8, hidden: true, mined: false },
+  { id: 9, hidden: true, mined: true },
+];
+
+console.log(initialArr);
+
 interface SquareProps {
-  name: any;
+  isHidden: any;
 }
 
 function Square(props: SquareProps) {
-  return <div className="square">{props.name}</div>;
+  const { isHidden } = props;
+  return (
+    <div
+      //  className="square"
+      className={isHidden ? "square-hidden" : "square"}
+    ></div>
+  );
 }
 
 function App() {
-  const squers = testArr.map((elem: any, index: any) => (
-    <Square key={index} name={elem} />
+  const squers = initialArr.map((elem: any) => (
+    <Square key={elem.id} isHidden={elem.hidden} />
   ));
 
   return (
@@ -20,10 +41,9 @@ function App() {
       <div className="board">
         <div className="squers">{squers}</div>
         <div className="gameInfo">
-        <div>you won!</div>
-        <button>reset</button>
+          <div>you won!</div>
+          <button>reset</button>
         </div>
-       
       </div>
     </div>
   );
